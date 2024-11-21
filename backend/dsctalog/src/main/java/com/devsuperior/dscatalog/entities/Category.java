5a49ed1d481a42bfd3630773af7a5012d83d0926
matheus albuquer <mj898926@gmail.com>
2024-11-21 20:01:49 -0300

    DTO

--- backend/dsctalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/dsctalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -12,10 +12,6 @@ import jakarta.persistence.Table;
 @Entity
 @Table (name = "tb_category")
 public class Category implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
